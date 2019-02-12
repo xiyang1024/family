86a55db1fd65dd2af275e416b083404edb7d84ee
xiyang1024 <47547816+xiyang1024@users.noreply.github.com>
2019-02-12 11:35:56 +0800

    Initial commit

---
+++ src/main/java/com/ycj/family/FamilyApplication.java
@@ -0,0 +1,14 @@
+package com.ycj.family;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyApplication.class, args);
+	}
+
+}
+
